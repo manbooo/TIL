3884c37de382f9e48b23f9e450be36ce7af35de7
jjuya <jjuya9381@naver.com>
2018-01-17 06:12:39 +0900

    setup virtualenv

---
+++ django/basic/웹 프레임워크 Django(python)/Django Basic with Code.md
@@ -0,0 +1,75 @@
+# Django Basic with Code
+
+### 부제 : 기초부터 탄탄히
+
+저자 : OneQ
+
+---
+
+### 프로젝트 & app 생성
+
+##### 0) 사전 준비(Windows)
+
+###### python 설치 및 버전 확인
+
+- <https://python.org/downloads/>
+
+```bash
+$ python --version
+```
+
+
+
+###### 가상환경 생성
+
+```bash
+$ python -m venv myenv
+```
+
+
+
+###### pip 최신 버전 업데이트 및 확인
+
+```bash
+(myenv) ~$ pip install --upgrade pip
+```
+
+
+
+###### Django 설치
+
+```bash
+(myenv) ~$ pip install django
+(myenv) ~$ django --version
+```
+
+
+
+---
+
+### 디렉토리 구조 확인
+
+
+
+---
+
+### 관리자 페이지 확인
+
+
+
+---
+
+### 글쓰기
+
+
+
+---
+
+### 리스트
+
+
+
+---
+
+### 글 보기
+
---
+++ django/basic/웹 프레임워크 Django(python)/myenv/Scripts/activate.bat
@@ -0,0 +1,32 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\JJu_Park\Desktop\Coding\django\basic\웹 프레임워크 Django(python)\myenv"
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+)
+
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(myenv) %PROMPT%"
+
+if defined PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+    set PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
