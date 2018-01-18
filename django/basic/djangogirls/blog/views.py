e188874e4815766bbf59cf6023e5c31af347fd86
jjuya <jjuya9381@naver.com>
2018-01-19 06:18:13 +0900

    djangogirls : Django template

--- django/basic/djangogirls/blog/views.py
+++ django/basic/djangogirls/blog/views.py
@@ -6,4 +6,4 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts' : posts})
