da65f6f1c3d6d61e00995488208e36461a4f4c8c
jjuya <jjuya9381@naver.com>
2018-01-20 03:49:48 +0900

    djangogirls : css

--- django/basic/djangogirls/blog/urls.py
+++ django/basic/djangogirls/blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.port_list, name = 'post_list'),
+    url(r'^$', views.post_list, name = 'post_list'),
 ]
