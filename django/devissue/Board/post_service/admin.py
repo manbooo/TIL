04fbaec7416a7ec517da05d602443cdc8fde2176
jjuya <jjuya9381@naver.com>
2018-02-15 02:10:20 +0900

    devissue django % REST framework : using paginator

--- django/devissue/Board/post_service/admin.py
+++ django/devissue/Board/post_service/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from post_service.models import Post
+
 # Register your models here.
+admin.site.register(Post)
--- django/devissue/Board/post_service/views.py
+++ django/devissue/Board/post_service/views.py
@@ -1,9 +1,26 @@
 from django.http.response import HttpResponse
 from django.template.loader import get_template
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+from post_service.models import Post
 # Create your views here.
-def post_list(resquest):
+def post_list(request):
     template = get_template('post_list.html')
-    context = {'python_word' : 'Python'}
+
+    page_data = Paginator(Post.objects.all(), 5)
+    page = request.GET.get('page', 1)
+
+    try:
+        posts = page_data.page(page)
+    except PageNotAnInteger:
+        posts = page_data.page(1)
+    except EmptyPage:
+        posts = page_data.page(page_data.num_pages)
+
+    context = {
+        'post_list' : posts,
+        'current_page' : page,
+        'total_page' : range(1, page_data.num_pages + 1)
+    }
 
     return HttpResponse(template.render(context))
