4a499c6f8e50c3b584f1bfdabbadb08994f7a587
jjuya <jjuya9381@naver.com>
2018-07-01 22:51:16 +0900

    생활코딩 WEB2 python_syntax_Boolean

---
+++ 생활코딩/WEB_2 python/htdocs/syntax/boolean.py
@@ -0,0 +1,24 @@
+#Number
+print(1) #Integer
+
+#String
+print('Hello world')
+
+#Boolean
+print(True) #참
+print(False) #거짓
+
+#Expression
+print(1+1)
+print('Hello '+'world') #Hello world
+
+#Comparison operator
+print(1==1) #True
+print(1<2) #True
+print(2<1) #False
+
+#Membership operator
+print('world' in 'Hello world') #True
+
+import os.path
+print(os.path.exists('boolean2.py'))
