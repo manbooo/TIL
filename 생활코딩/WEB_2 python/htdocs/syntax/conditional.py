1136a5dbc6a783f846c2c42a4ca439d9eb9e640f
jjuya <jjuya9381@naver.com>
2018-07-01 23:18:40 +0900

    생활코딩 WEB2 python_syntax_Conditional

---
+++ 생활코딩/WEB_2 python/htdocs/syntax/conditional.py
@@ -0,0 +1,17 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_pwd == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'egoing':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else:
+    print('Who are you?')
