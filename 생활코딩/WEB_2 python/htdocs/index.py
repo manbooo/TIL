e4dde8b3c577de5f60beb04f082863deead2cd32
jjuya <jjuya9381@naver.com>
2018-07-03 02:14:08 +0900

    생활코딩 WEB2 python_ex_Conditional and File open

--- 생활코딩/WEB_2 python/htdocs/index.py
+++ 생활코딩/WEB_2 python/htdocs/index.py
@@ -9,8 +9,10 @@ form = cgi.FieldStorage()
 
 if 'id' in form:
     pageId = form["id"].value
+    description = open('data/' + pageId, 'r').read()
 else:
     pageId = 'Welcome'
+    description = 'Hello, web'
 
 print('''
 <!doctype html>
@@ -25,10 +27,12 @@ print('''
         <li><a href="index.py?id=HTML">HTML</a></li>
         <li><a href="index.py?id=CSS">CSS</a></li>
         <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+        <li><a href="index.py?id=Python">Python</a></li>
       </ol>
       <h2>{title}</h2>
-        <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-        </p>
+      <p>
+        {desc}
+      </p>
     </body>
 </html>
-'''.format(title=pageId))
+'''.format(title = pageId, desc = description))
