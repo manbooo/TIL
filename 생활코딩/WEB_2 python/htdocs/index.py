83ec1c423c63e6de71aa9eb8240e85b273f59ca2
jjuya <jjuya9381@naver.com>
2018-07-03 23:48:07 +0900

    생활코딩 WEB2 python_syntax and ex_Loops

--- 생활코딩/WEB_2 python/htdocs/index.py
+++ 생활코딩/WEB_2 python/htdocs/index.py
@@ -3,7 +3,15 @@
 print("Content-Type: text/html") # HTML header
 print()
 
-import cgi
+import cgi, os
+
+files = os.listdir('data')
+#print(files)
+
+listStr = ''
+
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name = item)
 
 form = cgi.FieldStorage()
 
@@ -24,10 +32,7 @@ print('''
     <body>
       <h1><a href="index.py">WEB</a></h1>
       <ol>
-        <li><a href="index.py?id=HTML">HTML</a></li>
-        <li><a href="index.py?id=CSS">CSS</a></li>
-        <li><a href="index.py?id=JavaScript">JavaScript</a></li>
-        <li><a href="index.py?id=Python">Python</a></li>
+        {listStr}
       </ol>
       <h2>{title}</h2>
       <p>
@@ -35,4 +40,4 @@ print('''
       </p>
     </body>
 </html>
-'''.format(title = pageId, desc = description))
+'''.format(title = pageId, desc = description, listStr = listStr))
