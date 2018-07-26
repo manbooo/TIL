2c82367a161bf84dfdba92e7631a681dc766134f
jjuya <jjuya9381@naver.com>
2018-07-26 17:30:45 +0900

    Apply .gitignore

--- django/basic/djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jju_park\desktop\coding\django\djangogirls\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
