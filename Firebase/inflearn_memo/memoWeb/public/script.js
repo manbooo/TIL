297efedf4a26db6ce514302351a3ccd52d75e84f
jjuya <jjuya9381@naver.com>
2018-07-29 15:03:59 +0900

    Firebase_inflearn_memoWeb_database pring

--- Firebase/inflearn_memo/memoWeb/public/script.js
+++ Firebase/inflearn_memo/memoWeb/public/script.js
@@ -1,10 +1,14 @@
-var auth = firebase.auth();
+var auth, database, userInfo;
 var authProvider = new firebase.auth.GoogleAuthProvider();
+
+auth = firebase.auth();
 
 auth.onAuthStateChanged(function(user) {
   if(user) {
     // 인증 성공
     console.log("sucess!");
+
+    userInfo = user;
 
     // 메모 리스트 출력
   } else {
@@ -13,3 +17,48 @@ auth.onAuthStateChanged(function(user) {
     console.log("false!");
   }
 });
+
+database = firebase.database();
+
+function getMemoList() {
+  /*
+    {
+      memos : {
+        uid : {
+          text : "내용",
+          date : "작성일",
+          title : "제목"
+        },
+        uid : {
+          text : "내용",
+          date : "작성일",
+          title : "제목"
+        },
+        ...
+      }
+    }
+  */
+  var memoRef = database.ref('memos/' + userInfo.uid);
+
+  memoRef.on('child_added', on_child_added);
+}
+
+function on_child_added(data) {
+  //console.log(data.val());
+
+  var key = data.key;
+  var memoData = data.val();
+  var txt = memoData.txt;
+  var firstTxt = txt.substr(0, 1);
+  var title = memoData.title;
+
+  var html =
+         "<li id='" + key + "' class=\"collection-item avatar\" onclick=\"fn_get_data_one(this.id);\" >" +
+         "<i class=\"material-icons circle red\">" + firstTxt + "</i>" +
+         "<span class=\"title\">" + title + "</span>" +
+         "<p class='txt'>" + txt + "<br>" +
+         "</p>" +
+         "</li>";
+
+  $(".collection").append(html);
+}
