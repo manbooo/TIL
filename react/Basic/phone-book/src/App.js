1b06eabc6aa877c6f499f8267d8af50cda129cf9
jjuya <jjuya9381@naver.com>
2018-08-23 22:17:57 +0900

    React_basic_Manage Array : create and rendering

--- react/Basic/phone-book/src/App.js
+++ react/Basic/phone-book/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
 
+  id = 2
+
+  state = {
+    information: [
+      {
+        id: 0,
+        name: 'jju',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
   }
 
   render() {
+
     return (
       <div className="App">
         <PhoneForm
           onCreate={this.handleCreate}
         />
+
+        <PhoneInfoList data={this.state.information} />
       </div>
     );
   }
