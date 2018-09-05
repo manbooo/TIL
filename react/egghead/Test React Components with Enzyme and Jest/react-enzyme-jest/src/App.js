802dcbeaa134b00be64a0af6fb5bf4f50a4f5311
jjuya <jjuya9381@naver.com>
2018-09-05 13:07:53 +0900

    React_TDD in Enzyme and Jest_Shallow Rerander

--- react/egghead/Test React Components with Enzyme and Jest/react-enzyme-jest/src/App.js
+++ react/egghead/Test React Components with Enzyme and Jest/react-enzyme-jest/src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+          <ul className="tyler">
+            <li>test1</li>
+            <li>test2</li>
+            <li>test3</li>
+          </ul>
       </div>
     );
   }
--- react/egghead/Test React Components with Enzyme and Jest/react-enzyme-jest/src/App.test.js
+++ react/egghead/Test React Components with Enzyme and Jest/react-enzyme-jest/src/App.test.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { configure } from 'enzyme'
+import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// Shallow rendering
+describe('<App />', () => {
+    const wrapper = shallow(<App />)
+
+    it('should render App', () => {
+        console.log(wrapper.debug())
+    })
+
+    it('should contain 1 p', () => {
+        expect(wrapper.find('p').length).toBe(1)
+    })
+
+    it('should contain class name is App-intro', () => {
+        expect(wrapper.find('.App-intro').exists()).toBe(true)
+    })
+
+    it('should have 3 children in ul tag ', () => {
+        expect(wrapper.find('ul').children().length).toBe(3)
+    })
+
+    it('should have ul class name is tyler ', () => {
+        expect(wrapper.find('ul').hasClass('tyler')).toBe(true)
+    })
+
+})
+
