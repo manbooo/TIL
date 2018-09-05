fd610fecb4cd0bf1e927cbc863167395d8c54fe3
jjuya <jjuya9381@naver.com>
2018-09-05 11:00:13 +0900

    React_TDD in Enzyme and Jest_reSetup

--- react/egghead/Test React Components with Enzyme and Jest/react-enzyme-jest/src/App.test.js
+++ react/egghead/Test React Components with Enzyme and Jest/react-enzyme-jest/src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-configure({ adapter: new Adapter()})
+configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
