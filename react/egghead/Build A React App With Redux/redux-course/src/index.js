859cdc2d11464614438b52b883a73a54406c96a5
jjuya <jjuya9381@naver.com>
2018-09-05 19:51:48 +0900

    React_with Redux_Render React UI Based on State Changes with Subscribe in Redux

--- react/egghead/Build A React App With Redux/redux-course/src/index.js
+++ react/egghead/Build A React App With Redux/redux-course/src/index.js
@@ -6,7 +6,19 @@ import registerServiceWorker from './registerServiceWorker';
 
 import store from './store'
 
-const state = store.getState()
+const render = () => {
+    const state = store.getState()
 
-ReactDOM.render(<App {...state} />, document.getElementById('root'));
+    ReactDOM.render(<App {...state} />, document.getElementById('root'));
+}
+
+render()
+
+store.subscribe(render)
+
+setTimeout(() => {
+    store.dispatch({type: 'TODO_ADD', payload: {id: 4, name: 'test4', isChecked: false}})
+}, 1000)
+
+
 registerServiceWorker();
