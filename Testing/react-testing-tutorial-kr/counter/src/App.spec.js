1b4b9ada9639c1fd795e1ec518a2529973604063
jjuya <jjuya9381@naver.com>
2018-09-11 10:10:24 +0900

    TDD_react-testing-tutorial-kr_Enzyme

--- Testing/react-testing-tutorial-kr/counter/src/App.spec.js
+++ Testing/react-testing-tutorial-kr/counter/src/App.spec.js
@@ -1,4 +1,13 @@
-import { doIncrement, doDecrement } from './App'
+import App, { doIncrement, doDecrement, Counter } from './App'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { expect } from 'chai'
+import { mount, render, shallow,  configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
 
 describe('Local State', () => {
     // describe: 테스트의 묶음
@@ -9,13 +18,51 @@ describe('Local State', () => {
         const state = { counter: 0 }
         const newState = doIncrement(state)
 
-        expect(newState.counter).to.equal(1)
+        expect(newState.counter).toEqual(1)
     })
 
     it('should decrement the counter in state', () => {
         const state = { counter: 0 }
         const newState = doDecrement(state)
 
-        expect(newState.counter).to.equal(-1)
+        expect(newState.counter).toEqual(-1)
+    })
+})
+
+describe('App Component', () => {
+    it('Counter 래퍼를 그린다', () => {
+        const wrapper = shallow(<App />)
+
+        expect(wrapper.find(Counter).length).to.equal(1)
+    })
+
+    it('Counter 래퍼에 모든 Prop이 전달되었다', () => {
+        const wrapper = shallow(<App />)
+        let counterWrapper = wrapper.find(Counter)
+
+        wrapper.setState({ counter: 0 })
+        expect(counterWrapper.props().counter).to.equal(0)
+
+        wrapper.setState({ counter: -2 })
+        counterWrapper = wrapper.find(Counter)
+        expect(counterWrapper.props().counter).to.equal(-2)
+    })
+
+    it('Counter를 하나 올린다', () => {
+        const wrapper = shallow(<App />)
+
+        wrapper.setState({ counter: 0 })
+        wrapper.find('button').at(0).simulate('click')
+
+        expect(wrapper.state().counter).to.equal(1)
+    })
+
+    it('Counter를 하나 내린다', () => {
+        const wrapper = shallow(<App />)
+
+        wrapper.setState({ counter: 0 })
+        wrapper.find('button').at(1).simulate('click')
+
+        expect(wrapper.state().counter).to.equal(-1)
     })
 })
