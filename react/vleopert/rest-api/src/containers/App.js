8e37db95da597fb0c23a208720a1b7381ce73ffe
manbooo <jjuya9381@naver.com>
2018-09-15 15:20:49 +0900

    React_Velopert_Rest-api using json-server

---
+++ react/vleopert/rest-api/src/containers/App.js
@@ -0,0 +1,13 @@
+import Resct, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello MemoApp!
+            </div>
+        )
+    }
+}
+
+export default App
