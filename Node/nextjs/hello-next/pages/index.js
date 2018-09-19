0190b2a9dbb8252361ce22605654b3065e7f908d
manbooo <jjuya9381@naver.com>
2018-09-19 16:17:19 +0900

    Next_Document_page move Limk

--- Node/nextjs/hello-next/pages/index.js
+++ Node/nextjs/hello-next/pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 const Index = () => (
     <div>
+        <Link href="/about">
+            <button>About Page</button>
+        </Link>
         <p> Hello Next.js </p>
     </div>
 )
