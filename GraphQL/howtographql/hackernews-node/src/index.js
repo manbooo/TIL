e12f8d3ff1b0ab8127687928356afdeeecffed6e
manbooo <jjuya9381@naver.com>
2018-10-15 20:12:31 +0900

    GraphQL_server_a simple mutation

--- GraphQL/howtographql/hackernews-node/src/index.js
+++ GraphQL/howtographql/hackernews-node/src/index.js
@@ -1,38 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-    info : String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
-
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
     Query: {
         info: () => 'This is the API of a Hackernews Clone',
         feed: () => links,
     },
-    Link: {
-        id: (root) => root.id,
-        description: (root) => root.description,
-        url: (root) => root.url,
-    }
+    Mutation: {
+        post: (root, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+
+            return link
+        }
+    },
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 
