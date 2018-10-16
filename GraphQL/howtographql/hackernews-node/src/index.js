16457253f13647e5c3a3883edb3ec51bb6070ae5
manbooo <jjuya9381@naver.com>
2018-10-16 09:31:54 +0900

    GraphQL_server_connecting server and database with prosma binding

--- GraphQL/howtographql/hackernews-node/src/index.js
+++ GraphQL/howtographql/hackernews-node/src/index.js
@@ -1,35 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
     Query: {
         info: () => 'This is the API of a Hackernews Clone',
-        feed: () => links,
+        feed: () => (root, args, context, info) => {
+            return context.db.query.links({}, info)
+        },
     },
     Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            }
-            links.push(link)
-
-            return link
+        post: (root, args, context, info) => {
+            return context.db.mutation.createLink({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                }
+            })
         }
     },
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://eu1.prisma.sh/juyeon_park-2f06b1/graphql-tutorial/dev',
+            secret: 'mysecret123',
+            debug: true,
+        })
+    })
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
