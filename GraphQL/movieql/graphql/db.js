2842df6666560a8c2eacace33daf9f803530be98
manbooo <jjuya9381@naver.com>
2018-10-16 19:30:16 +0900

    GraphQL_Nomad_get movie and suggestoins

--- GraphQL/movieql/graphql/db.js
+++ GraphQL/movieql/graphql/db.js
@@ -1,20 +1,49 @@
 const axios = require('axios')
 
-const API_URL = "https://yts.am/api/v2/list_movies.json?"
+const BASE_URL = "https://yts.am/api/v2/"
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`
 
-let movies = []
-
-export const getMovies = (limit, minRating) => {
-    let REQUEST_URL = API_URL
-
-    if(limit > 0) {
-        REQUEST_URL += `limit=${limit}`
-    }
-    if(minRating > 0) {
-        REQUEST_URL += `&minimum_rating=${minRating}`
-    }
-
-    return axios.get(REQUEST_URL).then( response => {
-        return response.data.data.movies
+export const getMovies = async (limit, minRating) => {
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(LIST_MOVIES_URL, {
+        params: {
+            limit,
+            minimum_rating: minRating
+        }
     })
+
+    return movies
+}
+
+export const getMovie = async id => {
+    const {
+        data: {
+            data: { movie }
+        }
+    } = await axios(MOVIE_DETAILS_URL, {
+        params: {
+            movie_id: id
+        }
+    })
+
+    return movie
+}
+
+export const suggestions = async id => {
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(MOVIE_SUGGESTIONS_URL, {
+        params: {
+            movie_id: id
+        }
+    })
+
+    return movies
 }
