7c7d8e3083c05fe76f081648697048383cafe17c
manbooo <jjuya9381@naver.com>
2018-10-16 12:38:41 +0900

    GraphQL_server_filtering, pagination and sorting

--- GraphQL/howtographql/hackernews-node/src/index.js
+++ GraphQL/howtographql/hackernews-node/src/index.js
@@ -5,12 +5,14 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const AuthPayload = require('./resolvers/AuthPayload')
 const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
 
 const resolvers = {
     Query,
     Mutation,
     AuthPayload,
-    Subscription
+    Subscription,
+    Feed
 }
 
 const server = new GraphQLServer({
--- GraphQL/howtographql/hackernews-node/src/resolvers/Query.js
+++ GraphQL/howtographql/hackernews-node/src/resolvers/Query.js
@@ -1,5 +1,32 @@
 function feed(parent, args, context, info) {
-    return context.db.query.links({}, info)
+    const where = args.filter
+        ? {
+            OR: [
+                { url_contains: args.filter },
+                { description_contains: args.filter },
+            ],
+        }
+        : {}
+
+    const queriedLinks = await context.db.query.links(
+        { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+        `{ id }`,
+    )
+
+    const countSelectionSet = `
+    {
+        aggregate {
+            count
+        }
+    }
+    `
+
+    const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+    return {
+        count: linksConnection.aggregate.count,
+        linkIds: queriedLinks.map(link => link.id),
+    }
 }
 
 module.exports = {
