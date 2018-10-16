5b6c0fdb4dd3cebeb002be5b5172503578316a42
manbooo <jjuya9381@naver.com>
2018-10-16 17:32:57 +0900

    GraphQL_Nomad_get movies api using graphql

--- GraphQL/movieql/graphql/db.js
+++ GraphQL/movieql/graphql/db.js
@@ -1,53 +1,20 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Nicolas",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Jisu",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "Japan Guy",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Daal",
-        score: 2
+const axios = require('axios')
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?"
+
+let movies = []
+
+export const getMovies = (limit, minRating) => {
+    let REQUEST_URL = API_URL
+
+    if(limit > 0) {
+        REQUEST_URL += `limit=${limit}`
     }
-]
-
-export const getMovies = () => movies
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id)
-
-    return filteredMovies[0]
-}
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id)
-
-    if(movies.length > cleanedMovies.length) {
-        movies = cleanedMovies
-        return true
-    } else {
-        return false
-    }
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length + 1,
-        name,
-        score
+    if(minRating > 0) {
+        REQUEST_URL += `&minimum_rating=${minRating}`
     }
 
-    movies.push(newMovie)
-
-    return newMovie
+    return axios.get(REQUEST_URL).then( response => {
+        return response.data.data.movies
+    })
 }
