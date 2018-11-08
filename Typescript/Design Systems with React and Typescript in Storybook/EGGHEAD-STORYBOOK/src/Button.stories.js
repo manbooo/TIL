e052e50438de3fcb553f3d6759f85ce87205f0ac
manbooo <jjuya9381@naver.com>
2018-11-08 16:52:19 +0900

    Typescript & React & Storybook_egghead_Apply Style to a React StoryBook with the Info addon to Turn Stories into Documentation

--- Typescript/Design Systems with React and Typescript in Storybook/EGGHEAD-STORYBOOK/src/Button.stories.js
+++ Typescript/Design Systems with React and Typescript in Storybook/EGGHEAD-STORYBOOK/src/Button.stories.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { wInfo } from './utils'
+
 import { Button } from './Button'
 
 storiesOf('Button', module)
-    .addWithJSX('with background', () => <Button bg="palegoldenrod">Hello World</Button>)
+    .addWithJSX(
+        'with background',
+        wInfo(`
+      description
+      
+      ~~~js
+      <Button>slkdjslkdj</Button>
+      ~~~`)(() => <Button bg="palegoldenrod">Hello world</Button>),
+    )
     .addWithJSX('with background 2', () => <Button bg="green">Hello world2</Button>)
