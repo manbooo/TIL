7944175e1f90950a2c035852bce1308bc8532f06
manbooo <jjuya9381@naver.com>
2018-11-08 16:25:06 +0900

    Typescript & React & Storybook_egghead_Display the JSX of a React Storybook Story with the JSX Addon

--- Typescript/Design Systems with React and Typescript in Storybook/EGGHEAD-STORYBOOK/src/Button.stories.js
+++ Typescript/Design Systems with React and Typescript in Storybook/EGGHEAD-STORYBOOK/src/Button.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
 
 storiesOf('Button', module)
-    .add('with background', () => <Button bg="palegoldenrod">Hello World</Button>)
-    .add('with background 2', () => <Button bg="green">Hello world2</Button>)
+    .addWithJSX('with background', () => <Button bg="palegoldenrod">Hello World</Button>)
+    .addWithJSX('with background 2', () => <Button bg="green">Hello world2</Button>)
