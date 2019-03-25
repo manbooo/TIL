bab48e707b6d7388f3a7f5a47a507c3ddce3129d
manbooo <jjuya9381@naver.com>
2019-03-25 17:05:17 +0900

    next-config: add bundle-analyzer and css-loader, transpile-module

--- Node/nextjs/next-config/next.config.js
+++ Node/nextjs/next-config/next.config.js
@@ -1,6 +1,31 @@
-module.exports = {
-  webpack(config, options) {
-    // Further custom configuration here
-    return config
-  }
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+const withCss = require('@zeit/next-css')
+const withTM = require('next-transpile-modules')
+
+// fix: prevents error when .css files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = file => {}
 }
+
+module.exports = withBundleAnalyzer(
+  withCss(
+    withTM({
+      webpack(config, options) {
+        // Further custom configuration here
+        return config
+      },
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+			analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: './bundles/server.html'
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: './bundles/client.html'
+        }
+      }
+    })
+  )
+)
--- Node/nextjs/next-config/pages/index.js
+++ Node/nextjs/next-config/pages/index.js
@@ -1,1 +1,32 @@
-console.log('Hello world')
+import React from 'react'
+
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  onIncrease = () => {
+    this.setState(state => ({ count: state.count + 1 }))
+  }
+
+  onDecrease = () => {
+    this.setState(state => ({ count: state.count - 1 }))
+  }
+
+  render() {
+    // debugger
+
+    const { count } = this.state
+
+    return (
+      <div>
+        <h1>Hello World!!!</h1>
+        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
+        <button onClick={this.onIncrease}>+</button>
+        <button onClick={this.onDecrease}>-</button>
+      </div>
+    )
+  }
+}
+
+export default App
